56d04854aba7dda1f316aaac04ebd8f9fa3072b4
Оксана <kurufeenve@gmail.com>
2018-08-29 11:59:34 +0300

    first steps in Python

---
+++ ex2.py
@@ -0,0 +1,15 @@
+
+# a comment
+
+print("some text1")
+
+# a second comment
+
+print("some text2")
+
+# a third comment
+
+print("This '#' octoshit will be printed")
+
+#print("and this not")
+
---
+++ ex5.py
@@ -0,0 +1,18 @@
+
+my_name = 'Vladik'
+my_age = 28
+my_height = 183 #cm
+my_weight = 90 #kg
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm tall.")
+print(f"He's {my_weight} kg heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
